87cc1ebc7eecfa2a89bcca9b64e90320a9387110
Nishant Inamdar <nishant.inamdar@DM1379.datametica.com>
2021-11-25 16:16:52 +0530

    Palindrome.java file added

---
+++ palindrome.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+public class Palindrome {
+    public static void main(String[] args) {
+       String str = "";
+       for(String s:args)
+       {
+           str += s;
+       }
+       System.out.println("String "+str);
+       System.out.println("Length "+str.length());
+       System.out.println(("String in UpperCase "+str.toUpperCase()));
+
+       StringBuffer strbuf = new StringBuffer(str);
+       strbuf.reverse();
+       String StrRev = strbuf.toString();
+
+       if(str.equals(StrRev))
+           System.out.println("Palindrome");
+       else
+           System.out.println(" Not a Palindrome");
+    }
+}
